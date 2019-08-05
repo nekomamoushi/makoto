d39fbe7be92bea1fef627f0917d5e010a84a01b7
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 14:06:46 +0200

    feat(macos): add misc installation script

---
+++ src/os/macos/install/misc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../../lib/utils.sh"
+
+main () {
+    log_header "Misc"
+
+    cask_install "iina"
+    cask_install "numi"
+    cask_install "transmission"
+}
+
+main
