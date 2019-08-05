a8b45a165847c0561aa7d049c94bd895d7e5bb7f
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 13:05:46 +0200

    feat(macos): add homebrew installation script

---
+++ src/os/macos/install/homebrew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../../lib/utils.sh"
+
+homebrew_install () {
+    if ! has "brew" ; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        # Exit if, for some reason, Homebrew is not installed.
+        log_exit $? "Homebrew installation"
+    else
+        log_warn "Homebrew already installed"
+        return 0
+    fi
+}
