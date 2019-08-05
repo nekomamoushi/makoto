5d7f4fffb1823e1ea4245097caf57148a0b23eb0
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 13:58:14 +0200

    feat(macos): add dev installation script

---
+++ src/os/macos/install/development.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../../lib/utils.sh"
+
+main () {
+    log_header "Development"
+
+    cask_install "iterm2"
+    cask_install "sublime-merge"
+
+    cask_install "android-studio"
+    cask_install "android-platform-tools"
+    cask_install "android-sdk"
+    cask_install "adoptopenjdk8"
+
+
+    brew_install "python@2"
+    brew_install "python"
+    brew_install "pyenv"
+    brew_install "pipenv"
+
+    brew_install "nvm"
+}
+
+main
