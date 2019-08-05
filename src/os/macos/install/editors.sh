1ef0863035775f27c49fead249290661afad1e78
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 14:00:43 +0200

    feat(macos): add editors installation script

---
+++ src/os/macos/install/editors.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../../lib/utils.sh"
+
+main () {
+    log_header "Misc"
+
+    cask_install "sublime-text"
+    cask_install "vscodium"
+    cask_install "macdown"
+}
+
+main
