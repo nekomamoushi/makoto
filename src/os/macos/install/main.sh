c505dc3c4afcbc0bb5d39f2401d2149d4514e5c8
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 16:33:47 +0200

    fix(macos): call script instead of source script

--- src/os/macos/install/main.sh
+++ src/os/macos/install/main.sh
@@ -4,15 +4,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-. "./homebrew.sh"
-. "./shells.sh"
-. "./fonts.sh"
-. "./git.sh"
-. "./editors.sh"
-. "./development.sh"
-. "./tools.sh"
-. "./browsers.sh"
-. "./productivity.sh"
-. "./misc.sh"
-. "./quicklook.sh"
+./homebrew.sh
+./shells.sh
+./fonts.sh
+./git.sh
+./editors.sh
+./development.sh
+./tools.sh
+./browsers.sh
+./productivity.sh
+./misc.sh
+./quicklook.sh
 
