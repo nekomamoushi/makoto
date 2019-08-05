af4252b5209e4a72deb29611fa6cfb9d10de7db3
Nekomamoushi <germione.grangere@gmail.com>
2019-08-05 13:42:43 +0200

    feat(macos): add fonts installation script

---
+++ src/os/macos/install/fonts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && . "../../../../lib/utils.sh"
+
+main () {
+    log_header "Fonts"
+
+    cask_install "font-iosevka"
+    cask_install "font-fira-code"
+}
+
+main
